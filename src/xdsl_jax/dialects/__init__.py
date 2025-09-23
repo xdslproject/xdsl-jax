4b2d6f47eb56fb3ada68eb699ab937d316564913
Sasha Lopoukhine <superlopuh@gmail.com>
2025-09-23 23:52:52 +0100

    dialects: (stablehlo) copy over stablehlo dialect
    Co-authored-by: Sasha Lopoukhine <superlopuh@gmail.com>
    Co-authored-by: Erick Ochoa <github@ceci-nest-pas.me>
    Co-authored-by: Spica <spicaofsky@gmail.com>
    Co-authored-by: erick-xanadu <110487834+erick-xanadu@users.noreply.github.com>
    Co-authored-by: Fehr Mathieu <mathieu.fehr@gmail.com>
    Co-authored-by: Chris Vasiladiotis <cvassiladiotis@gmail.com>
    Co-authored-by: Alex Rice <alexrice999@hotmail.co.uk>

--- src/xdsl_jax/dialects/__init__.py
+++ src/xdsl_jax/dialects/__init__.py
@@ -6,5 +6,11 @@ from xdsl.ir import Dialect
 def get_all_dialects() -> dict[str, Callable[[], Dialect]]:
     """Returns all available dialects."""
 
-    # Add your dialects here to be discovered by `xdsl-opt`
-    return {}
+    def get_stablehlo():
+        from xdsl.dialects.stablehlo import StableHLO
+
+        return StableHLO
+
+    return {
+        "stablehlo": get_stablehlo,
+    }
