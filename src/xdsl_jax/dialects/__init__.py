7e9866bb9fc82b110ee1a032deaf52415400f301
Sasha Lopoukhine <superlopuh@gmail.com>
2025-09-28 09:20:38 +0100

    use our own stablehlo dialect

--- src/xdsl_jax/dialects/__init__.py
+++ src/xdsl_jax/dialects/__init__.py
@@ -7,7 +7,7 @@ def get_all_dialects() -> dict[str, Callable[[], Dialect]]:
     """Returns all available dialects."""
 
     def get_stablehlo():
-        from xdsl.dialects.stablehlo import StableHLO
+        from xdsl_jax.dialects.stablehlo import StableHLO
 
         return StableHLO
 
