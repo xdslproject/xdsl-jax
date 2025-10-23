b2c0f7c4013efa950910ef3afa23e242b2457cef
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2025-10-23 13:07:17 -0400

    Moving current attributes out into a separate file

--- src/xdsl_jax/dialects/stablehlo.py
+++ src/xdsl_jax/dialects/stablehlo.py
@@ -12,15 +12,12 @@ from typing import ClassVar, TypeAlias, cast
 
 from xdsl.dialects.builtin import (
     I32,
-    I64,
     AnyFloat,
     AnyTensorType,
     AnyTensorTypeConstr,
-    ArrayAttr,
     ComplexType,
     DenseArrayBase,
     DenseIntOrFPElementsAttr,
-    IntegerAttr,
     IntegerType,
     TensorType,
     i64,
@@ -28,13 +25,8 @@ from xdsl.dialects.builtin import (
 from xdsl.ir import (
     Attribute,
     Dialect,
-    EnumAttribute,
-    ParametrizedAttribute,
     Region,
-    SpacedOpaqueSyntaxAttribute,
     SSAValue,
-    StrEnum,
-    TypeAttribute,
 )
 from xdsl.irdl import (
     AnyAttr,
@@ -43,7 +35,6 @@ from xdsl.irdl import (
     VarConstraint,
     attr_def,
     base,
-    irdl_attr_definition,
     irdl_op_definition,
     operand_def,
     result_def,
@@ -52,10 +43,16 @@ from xdsl.irdl import (
     var_region_def,
     var_result_def,
 )
-from xdsl.parser import AttrParser
-from xdsl.printer import Printer
 from xdsl.traits import IsTerminator
 from xdsl.utils.exceptions import VerifyException
+
+from xdsl_jax.dialects.attributes import (
+    ComparisonDirectionAttr,
+    ComparisonTypeAttr,
+    DotAttr,
+    PrecisionAttr,
+    TokenType,
+)
 
 IntegerTensorType: TypeAlias = TensorType[IntegerType]
 FloatOrComplexType: TypeAlias = AnyFloat | ComplexType
@@ -155,235 +152,6 @@ class FloatTensorLikeElementwiseUnaryOperation(IRDLOperation, abc.ABC):
         if result_type is None:
             result_type = operand.type
         super().__init__(operands=(operand,), result_types=(result_type,))
-
-
-# endregion
-
-# region Attributes
-
-
-class ComparisonDirection(StrEnum):
-    """
-    Comparison direction for stablehlo.
-    """
-
-    EQ = "EQ"
-    NE = "NE"
-    GE = "GE"
-    GT = "GT"
-    LE = "LE"
-    LT = "LT"
-
-
-@irdl_attr_definition
-class ComparisonDirectionAttr(
-    EnumAttribute[ComparisonDirection], SpacedOpaqueSyntaxAttribute
-):
-    """
-    The values of `comparison_direction` and `compare_type` have the following
-    semantics:
-    For boolean and integer element types:
-
-    * `EQ`: lhs = rhs.
-    * `NE`: lhs != rhs.
-    * `GE`: lhs >= rhs.
-    * `GT`: lhs > rhs.
-    * `LE`: lhs <= rhs
-    * `LT`: lhs < rhs.
-
-    For floating-point element types with `compare_type = FLOAT`, the op implements the
-    following IEEE-754 operations:
-
-    * `EQ`: compareQuietEqual.
-    * `NE`: compareQuietNotEqual.
-    * `GE`: compareQuietGreaterEqual.
-    * `GT`: compareQuietGreater.
-    * `LE`: compareQuietLessEqual.
-    * `LT`: compareQuietLess.
-
-    For floating-point element types with `compare_type = TOTALORDER`, the op uses the
-    combination of totalOrder and compareQuietEqual operations from IEEE-754.
-    For complex element types, lexicographic comparison of (real, imag) pairs is
-    performed using the provided `comparison_direction` and `compare_type`.
-    Imposing an ordering on complex numbers involves surprising semantics, so in the
-    future we are planning to remove support for complex numbers when
-    comparison_direction is `GE`, `GT`, `LE` or `LT`.
-
-    For quantized types, performs `dequantize_compare(lhs, rhs, comparison_direction)`
-
-    See external [documentation](https://github.com/openxla/stablehlo/blob/main/docs/spec.md#compare).
-    """
-
-    name = "stablehlo.comparison_direction"
-
-
-class ComparisonType(StrEnum):
-    """
-    Together with `comparison_direction` determines the semantics of comparison.
-
-    See StableHLO's [source](https://github.com/openxla/stablehlo/blob/500b49459b2bd2f822644d4d9a43109e537d51e4/stablehlo/dialect/StablehloEnums.td#L152-L156).
-    """
-
-    NOTYPE = "NOTYPE"
-    FLOAT = "FLOAT"
-    TOTALORDER = "TOTALORDER"
-    SIGNED = "SIGNED"
-    UNSIGNED = "UNSIGNED"
-
-
-@irdl_attr_definition
-class ComparisonTypeAttr(EnumAttribute[ComparisonType], SpacedOpaqueSyntaxAttribute):
-    """
-    Together with `comparison_direction` determines the semantics of comparison.
-
-    See external [documentation](https://github.com/openxla/stablehlo/blob/main/docs/spec.md#compare).
-    """
-
-    name = "stablehlo.comparison_type"
-
-
-class Precision(StrEnum):
-    """
-    XLA precision for an operand. Has backend specific meaning.
-    """
-
-    DEFAULT = "DEFAULT"
-    HIGH = "HIGH"
-    HIGHEST = "HIGHEST"
-
-
-@irdl_attr_definition
-class PrecisionAttr(EnumAttribute[Precision], SpacedOpaqueSyntaxAttribute):
-    """
-    XLA precision for an operand. Has backend specific meaning.
-
-    See external [documentation](https://github.com/openxla/stablehlo/blob/b075e948092d8a27ed0be48f4f8dbaa6df7e2e3e/stablehlo/dialect/StablehloEnums.td#L46).
-    """
-
-    name = "stablehlo.precision"
-
-
-@irdl_attr_definition
-class TokenType(TypeAttribute, ParametrizedAttribute):
-    """
-    Token types represent tokens, i.e. opaque values produced and consumed by some
-    operations.
-    Tokens are used for imposing execution order on operations as described in the
-    Execution section.
-
-    E.g.,
-
-    ```mlir
-      // %input0: !stablehlo.token
-      // %input1: !stablehlo.token
-      %result = "stablehlo.after_all"(%input0, %input1)
-              : (!stablehlo.token, !stablehlo.token) -> !stablehlo.token
-    ```
-    """
-
-    name = "stablehlo.token"
-
-
-@irdl_attr_definition
-class DotAttr(ParametrizedAttribute):
-    """
-    Attribute that models the dimension information for dot.
-
-    See external [documentation](https://github.com/openxla/stablehlo/blob/b075e948092d8a27ed0be48f4f8dbaa6df7e2e3e/stablehlo/dialect/StablehloAttrs.td#L82).
-    """
-
-    name = "stablehlo.dot"
-
-    lhs_batching_dimensions: ArrayAttr[IntegerAttr[I64]]
-    rhs_batching_dimensions: ArrayAttr[IntegerAttr[I64]]
-    lhs_contracting_dimensions: ArrayAttr[IntegerAttr[I64]]
-    rhs_contracting_dimensions: ArrayAttr[IntegerAttr[I64]]
-
-    @staticmethod
-    def _print_parameter(
-        name: str, value: ArrayAttr[IntegerAttr[I64]], printer: Printer
-    ):
-        printer.print_string(f"\n{name} = [")
-        printer.print_list(
-            value.data,
-            lambda dim: printer.print_string(f"{dim.value.data}"),
-        )
-        printer.print_string("]")
-
-    @staticmethod
-    def _parse_parameter(
-        name: str, parser: AttrParser, optional: bool = False
-    ) -> ArrayAttr[IntegerAttr[I64]]:
-        if optional:
-            if parser.parse_optional_characters(name) is None:
-                return ArrayAttr(())
-        else:
-            parser.parse_characters(name)
-        parser.parse_punctuation("=")
-        value = parser.parse_comma_separated_list(
-            AttrParser.Delimiter.SQUARE,
-            lambda: IntegerAttr(parser.parse_integer(), i64),
-        )
-        return ArrayAttr(value)
-
-    def print_parameters(self, printer: Printer) -> None:
-        with printer.in_angle_brackets():
-            with printer.indented():
-                if (
-                    self.lhs_batching_dimensions.data
-                    and self.rhs_batching_dimensions.data
-                ):
-                    DotAttr._print_parameter(
-                        "lhs_batching_dimensions", self.lhs_batching_dimensions, printer
-                    )
-                    printer.print_string(",")
-                    DotAttr._print_parameter(
-                        "rhs_batching_dimensions", self.rhs_batching_dimensions, printer
-                    )
-                    printer.print_string(",")
-
-                DotAttr._print_parameter(
-                    "lhs_contracting_dimensions",
-                    self.lhs_contracting_dimensions,
-                    printer,
-                )
-                printer.print_string(",")
-                DotAttr._print_parameter(
-                    "rhs_contracting_dimensions",
-                    self.rhs_contracting_dimensions,
-                    printer,
-                )
-            printer.print_string("\n")
-
-    @classmethod
-    def parse_parameters(cls, parser: AttrParser) -> Sequence[Attribute]:
-        with parser.in_angle_brackets():
-            lhs_batching_dimensions = DotAttr._parse_parameter(
-                "lhs_batching_dimensions", parser, optional=True
-            )
-            if lhs_batching_dimensions.data:
-                parser.parse_punctuation(",")
-                rhs_batching_dimensions = DotAttr._parse_parameter(
-                    "rhs_batching_dimensions", parser
-                )
-                parser.parse_punctuation(",")
-            else:
-                rhs_batching_dimensions = ArrayAttr(())
-
-            lhs_contracting_dimensions = DotAttr._parse_parameter(
-                "lhs_contracting_dimensions", parser
-            )
-            parser.parse_punctuation(",")
-            rhs_contracting_dimensions = DotAttr._parse_parameter(
-                "rhs_contracting_dimensions", parser
-            )
-
-            return (
-                lhs_batching_dimensions,
-                rhs_batching_dimensions,
-                lhs_contracting_dimensions,
-                rhs_contracting_dimensions,
-            )
 
 
 # endregion
