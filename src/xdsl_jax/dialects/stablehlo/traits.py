130d033e315a524191c8173ed3aa786487df6d9f
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2026-03-03 11:43:57 -0500

    add CompatibleOperandsAndResultType trait

--- src/xdsl_jax/dialects/stablehlo/traits.py
+++ src/xdsl_jax/dialects/stablehlo/traits.py
@@ -4,9 +4,45 @@ Traits specific to the StableHLO dialect.
 
 from typing import cast
 
-from xdsl.dialects.builtin import DYNAMIC_INDEX, TensorType
-from xdsl.ir import Operation
-from xdsl.traits import ConditionallySpeculatable, RecursivelySpeculatable
+from xdsl.dialects.builtin import DYNAMIC_INDEX, ShapedType, TensorType
+from xdsl.ir import Attribute, Operation
+from xdsl.traits import ConditionallySpeculatable, OpTrait, RecursivelySpeculatable
+from xdsl.utils.exceptions import VerifyException
+from xdsl.utils.type import get_element_type_or_self, have_compatible_shape
+
+
+def _is_compatible_element_type(lhs: Attribute, rhs: Attribute) -> bool:
+    """Verify that the element types of the two types are compatible."""
+    return get_element_type_or_self(lhs) == get_element_type_or_self(rhs)
+
+
+def _is_compatible(lhs: Attribute, rhs: Attribute) -> bool:
+    """Verify that the two types are compatible."""
+    if isinstance(lhs, ShapedType) and isinstance(rhs, ShapedType):
+        return have_compatible_shape(lhs, rhs) and _is_compatible_element_type(lhs, rhs)
+    return _is_compatible_element_type(lhs, rhs)
+
+
+class CompatibleOperandsAndResultType(OpTrait):
+    def verify(self, op: Operation) -> None:
+        """Verify that the operation has compatible types for all operands/results."""
+        expected: Attribute | None = None
+        if op.results:
+            expected = op.result_types[0]
+        if op.operands:
+            expected = op.operand_types[0]
+        if expected is None:
+            raise VerifyException(
+                f"'{op.name}' requires at least one operand or result for "
+                "CompatibleOperandsAndResultType"
+            )
+
+        all_types = tuple(op.operand_types) + tuple(op.result_types)
+        for actual in all_types:
+            if not _is_compatible(actual, expected):
+                raise VerifyException(
+                    f"'{op.name}' requires compatible types for all operands/results"
+                )
 
 
 class RecursivelySpeculatableIfStaticDimInOutputIsStaticInInput(
