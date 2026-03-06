0aaaf85369b2e38e1f25020ee8a5413637bb9038
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2026-03-06 15:22:39 -0500

    Add Reshape operation

--- src/xdsl_jax/dialects/stablehlo/data_movement_ops.py
+++ src/xdsl_jax/dialects/stablehlo/data_movement_ops.py
@@ -2,7 +2,8 @@
 Data movement operations for the StableHLO dialect.
 """
 
-from xdsl.dialects.builtin import AnyTensorType, DenseArrayBase, i64
+from xdsl.dialects.builtin import AnyTensorType, DenseArrayBase, TensorType, i64
+from xdsl.ir import Operation, cast
 from xdsl.irdl import (
     IRDLOperation,
     irdl_op_definition,
@@ -15,6 +16,7 @@ from xdsl.traits import (
     ConditionallySpeculatable,
     NoMemoryEffect,
 )
+from xdsl.utils.exceptions import VerifyException
 
 from xdsl_jax.xdsl_extras import (
     AllMatchSameOperatorTrait,
@@ -23,6 +25,67 @@ from xdsl_jax.xdsl_extras import (
 
 from .custom_directives import SliceRanges
 from .traits import SpeculatableIfStaticDimInOutputIsStaticInInput
+
+
+@irdl_op_definition
+class ReshapeOp(IRDLOperation):
+    """
+    Performs reshape of ``operand`` tensor to a ``result`` tensor.
+
+    See:
+    https://github.com/openxla/stablehlo/blob/main/docs/spec.md#reshape
+
+    Example:
+    ```mlir
+    %result = stablehlo.reshape %operand : (tensor<2xf32>) -> tensor<1x2xf32>
+    ```
+    """
+
+    name = "stablehlo.reshape"
+    operand = operand_def(AnyTensorType)
+    result = result_def(AnyTensorType)
+
+    assembly_format = """
+    operands attr-dict `:` functional-type(operands, results)
+    """
+
+    traits = traits_def(
+        NoMemoryEffect(),
+        SameOperandsAndResultElementType(),
+    )
+
+    @classmethod
+    def is_speculatable(cls, op: Operation) -> bool:
+        operand_type = op.operand_types[0]
+        return isinstance(operand_type, TensorType) and operand_type.has_static_shape()
+
+    def verify_(self) -> None:
+        """Verify that the operation has the same shape for all operands and results."""
+        o_type = cast(TensorType, self.operand_types[0])
+        r_type = cast(TensorType, self.result_types[0])
+
+        # If o_type or r_type is dynamically shaped there is nothing to verify.
+        if not o_type.has_static_shape() or not r_type.has_static_shape():
+            return
+
+        # If the operand type is statically shaped (not required) the number of
+        # elements must match that of the result type.
+        num_operand_elements = 1
+        for dim in o_type.get_shape():
+            num_operand_elements *= dim
+
+        num_result_elements = 1
+        for dim in r_type.get_shape():
+            num_result_elements *= dim
+
+        if num_result_elements != num_operand_elements:
+            raise VerifyException(
+                "number of output elements ("
+                f"{num_result_elements}"
+                ") doesn't match expected number of elements ("
+                f"{num_operand_elements}"
+                ")"
+            )
 
 
 @irdl_op_definition
