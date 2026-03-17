cb05442d4d32962a04813ab6343733de19234f05
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2026-03-17 10:39:17 -0400

    add option to print in generic format

--- tests/jax_roundtrip.py
+++ tests/jax_roundtrip.py
@@ -8,16 +8,21 @@ and prints it back out to validate compatibility with JAX's StableHLO implementa
 
 import sys
 from pathlib import Path
+from typing import no_type_check
 
 from jax._src.interpreters import mlir as jax_mlir
 
 
-def ir_roundtrip(mlir_text: str) -> str:
-    with jax_mlir.make_ir_context() as ctx:  # pyright: ignore
+@no_type_check
+def ir_roundtrip(mlir_text: str, print_generic_format: bool = False) -> str:
+    with jax_mlir.make_ir_context() as ctx:
         ctx.allow_unregistered_dialects = True
-        module = jax_mlir.ir.Module.parse(mlir_text)  # pyright: ignore
-        output = module.operation.get_asm(large_elements_limit=20)  # pyright: ignore
-        return output  # pyright: ignore
+        module = jax_mlir.ir.Module.parse(mlir_text)
+        output = module.operation.get_asm(
+            large_elements_limit=20,
+            print_generic_op_form=print_generic_format,
+        )
+        return output
 
 
 def main():
@@ -27,6 +32,11 @@ def main():
         description="JAX StableHLO roundtrip test script. "
         "Reads MLIR text (from a file or stdin), parses it using JAX's MLIR API, "
         "and prints it back out to validate."
+    )
+    parser.add_argument(
+        "--print-generic",
+        action="store_true",
+        help="Print the result using generic op form.",
     )
     parser.add_argument(
         "input_file",
@@ -40,7 +50,7 @@ def main():
         sys.stdin.read() if args.input_file is None else args.input_file.read_text()
     )
 
-    print(ir_roundtrip(mlir_text))
+    print(ir_roundtrip(mlir_text, print_generic_format=args.print_generic))
 
 
 if __name__ == "__main__":
